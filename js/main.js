3d19caaa9bed66d7458c5cda55b5a89067885827
Olga Shtareva <olga-shtar@ya.ru>
2021-03-20 22:32:34 +0300

    create: modal

--- js/main.js
+++ js/main.js
@@ -31,3 +31,24 @@ const menuButton = document.querySelector('.menu-button');
 menuButton.addEventListener('click', function () {
   document.querySelector('.navbar-bottom').classList.toggle('navbar-bottom--visible');
 })
+
+const modalBtns = document.querySelectorAll('[data-toggle=modal]');
+const modalClose = document.querySelector('.modal__close');
+const modalOverlay = document.querySelector('.modal__overlay');
+const modalDialog = document.querySelector('.modal__dialog');
+modalBtns.forEach(btn => {
+  btn.addEventListener('click', () => {
+    modalOverlay.classList.add('modal__overlay--visible')
+    modalDialog.classList.add('modal__dialog--visible')
+  })
+});
+modalClose.addEventListener('click', () => {
+  modalOverlay.classList.remove('modal__overlay--visible')
+  modalDialog.classList.remove('modal__dialog--visible')
+})
+document.addEventListener('keydown', (e) => {
+  if (e.code = 'Escape') {
+    modalOverlay.classList.remove('modal__overlay--visible')
+    modalDialog.classList.remove('modal__dialog--visible')    
+  }
+})
